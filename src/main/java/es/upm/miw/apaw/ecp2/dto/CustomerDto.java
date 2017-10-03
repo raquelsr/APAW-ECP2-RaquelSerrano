647c5f5366136f86cedae51d85620677b128a77c
Raquel <Raquel@air-de-raquel.home>
2017-10-03 21:44:42 +0200

    Controllers

---
+++ src/main/java/es/upm/miw/apaw/ecp2/dto/CustomerDto.java
@@ -0,0 +1,39 @@
+package es.upm.miw.apaw.ecp2.dto;
+
+import es.upm.miw.apaw.ecp2.entities.Customer;
+
+public class CustomerDto {
+    
+    private int id;
+
+    private String name;
+
+    public CustomerDto() {
+    }
+
+    public CustomerDto(Customer customer) {
+        id = customer.getId();
+        name = customer.getName();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "{\"id\":" + id + ",\"name\":\"" + name + "\"}";
+    }
+}
