5c57b7d326ca2be1ce0a527df8afdcc405807570
Raquel <Raquel@air-de-raquel.home>
2017-10-03 19:39:41 +0200

    HTTP Package

---
+++ src/main/java/es/upm/miw/apaw/ecp2/api/Dispatcher.java
@@ -0,0 +1,33 @@
+package es.upm.miw.apaw.ecp2.api;
+
+import es.upm.miw.apaw.ecp2.http.HttpRequest;
+import es.upm.miw.apaw.ecp2.http.HttpResponse;
+
+public class Dispatcher {
+
+    public void doPost(HttpRequest request, HttpResponse response) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void doGet(HttpRequest request, HttpResponse response) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void doPatch(HttpRequest request, HttpResponse response) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void doPut(HttpRequest request, HttpResponse response) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void doDelete(HttpRequest request, HttpResponse response) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
